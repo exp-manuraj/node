c22029f66878d1c280378bb46cf19f037f173e76
rinirinku <rini.cr@experionglobal.com>
2017-02-05 00:10:43 +0530

    questions

--- app.module.js
+++ app.module.js
@@ -2,7 +2,7 @@
 
 const dependencies = [
     'auth',
-    'items'
+    'questions'
 ];
 
 
--- modules/questions/questions.controller.js
+++ modules/questions/questions.controller.js
@@ -1,4 +1,6 @@
-
+var Learnosity = require('learnosity-sdk-nodejs');
+const config = require('../../config');
+var request = require('request');
 /**
  * @api {GET} /questions List questions
  * @apiName List
@@ -10,6 +12,36 @@
  * @apiSuccess {String} lastname  Lastname of the User.
  */
 function list(req, res) {
+	var limit = 20;
+	var url = 'https://data.learnosity.com/latest/itembank/items';
+	var learnositySdk = new Learnosity();
+	const consumer_key = config.consumer_key;
+	const secret_key = config.secret_key;
+	if (_.isEmpty(req.query.limit)) {
+    	limit = 20;
+    } else {
+    	limit = req.query.limit;
+    }
+	var signature = learnositySdk.init('data', {
+            'consumer_key': consumer_key,
+            'domain': domain
+        },
+        secret_key, {
+            "limit": limit
+        },
+        'get'
+    );
+    var options = {
+        url: url,
+        form: signature
+    }
+    //making the https get call
+    request.post(options, function(err, httpResponse1, body1) {
+        var result = JSON.parse(body1);
+    });
+    console.log(result);
+
+
 
 }
 
